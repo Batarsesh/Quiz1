0350a7c2d47fb0c4e9730baade035df4543cd152
Batarsesh <dani.b.11@hotmail.com>
2023-06-16 18:50:28 -0600

    Se agrega borrar n nodos

--- Lista_Simple/Capa_Logica/Lista_Simple/Orquestador.cs
+++ Lista_Simple/Capa_Logica/Lista_Simple/Orquestador.cs
@@ -6,6 +6,18 @@ namespace Capa_Logica.Lista_Simple
     {
         private NodoInt cabeza;
 
+        public bool Cabeza_No_Nula()
+        {
+            if (cabeza != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         public void Agregar_NodoInt(int valor) {
 
             NodoInt nodoNuevo = new NodoInt(valor);
@@ -33,15 +45,71 @@ namespace Capa_Logica.Lista_Simple
 
             NodoInt nodoActual = cabeza;
 
-            if (nodoActual !=null)
-            {               
+            if (Cabeza_No_Nula())
+            {
+                Console.WriteLine("***Empieza Lista***");
                 while (nodoActual!=null)
                 {
                     int valorActual = nodoActual.Valor;
                     Console.WriteLine(valorActual.ToString());
                     nodoActual = nodoActual.Siguiente;
                 }
+                Console.WriteLine("***Termina Lista***");
             }
+
+        }
+
+        public int Busque_Valor(int _valor_A_Buscar)
+        {
+            int cantidad = 0;
+
+            if (Cabeza_No_Nula())
+            {
+                NodoInt nodoActual = cabeza;
+
+                while (nodoActual != null)
+                {
+                    if (nodoActual.Valor == _valor_A_Buscar)
+                    {
+                        cantidad++;
+                    }
+                    nodoActual = nodoActual.Siguiente;
+                }
+            }
+            return cantidad;
+        }
+        public void Elimine_Nodo()
+        {
+            if (Cabeza_No_Nula())
+            {
+                NodoInt nodoActual = cabeza;
+                NodoInt aux = cabeza;
+                nodoActual = nodoActual.Siguiente;
+                aux = null;
+                cabeza = nodoActual;
+            }
+        }
+
+        public void Borrar_N_Nodos(int valorPosicion, int cantEliminar)
+        {
+            NodoInt nodoActual = cabeza;
+            while (cantEliminar > 0)
+            {
+                if (nodoActual.Siguiente != null && valorPosicion == nodoActual.Valor)
+                {
+                    Console.WriteLine("Se borro el valor: " + valorPosicion + "\nQuedan " + cantEliminar + " restantes.");
+                    Elimine_Nodo();
+                    cantEliminar--;
+                }
+                else
+                {
+                    Console.WriteLine("No se ha borrado ningun valor.");
+                    cantEliminar--;
+                }
+                nodoActual = nodoActual.Siguiente;
+            }
+
+            cantEliminar = 0;
 
         }
     }
--- Lista_Simple/Capa_Presentacion/Program.cs
+++ Lista_Simple/Capa_Presentacion/Program.cs
@@ -2,8 +2,12 @@
 using Capa_Modelo.Nodo;
 
 Orquestador orquestador = new Orquestador();
-orquestador.Agregar_NodoInt(1);
-orquestador.Agregar_NodoInt(2);
-orquestador.Agregar_NodoInt(3);
-orquestador.Agregar_NodoInt(4);
+orquestador.Agregar_NodoInt(12);
+orquestador.Agregar_NodoInt(21);
+orquestador.Agregar_NodoInt(21);
+orquestador.Agregar_NodoInt(33);
+orquestador.Agregar_NodoInt(21);
+orquestador.Mostrar_Lista_Simple();
+
+orquestador.Borrar_N_Nodos(21,3);
 orquestador.Mostrar_Lista_Simple();
